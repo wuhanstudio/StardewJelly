9170119e48004f43c2bf29927e34e3325b1c3603
wuhanstudio <wuhanstudios@gmail.com>
2026-02-22 10:27:26 +0000

    [player] Move update and draw functinos inside player class

--- Game1.cs
+++ Game1.cs
@@ -25,36 +25,12 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
     
     private OrthographicCamera _camera;
-    
-    private Vector2 PLAYER_OFFSET = new Vector2(48, 48);
-    
-    Texture2D playerSprite;
-    
-    Texture2D walkUp;
-    Texture2D walkDown;
-    Texture2D walkLeft;
-    Texture2D walkRight;
-    
-    private Texture2DAtlas walk_up_atlas;
-    private Texture2DAtlas walk_down_atlas;
-    private Texture2DAtlas walk_left_atlas;
-    private Texture2DAtlas walk_right_atlas;
-    
-    private SpriteSheet walk_up_sprite_sheet;
-    private SpriteSheet walk_down_sprite_sheet;
-    private SpriteSheet walk_left_sprite_sheet;
-    private SpriteSheet walk_right_sprite_sheet;
 
-    private AnimationController walkUpAnimationController;
-    private AnimationController walkDownAnimationController;
-    private AnimationController walkLeftAnimationController;
-    private AnimationController walkRightAnimationController;
+    private Texture2D _background;
+    private Texture2D ball;
+    private Texture2D skull;
     
-    Texture2D background;
-    Texture2D ball;
-    Texture2D skull;
-    
-    Player player = new Player();
+    Player player;
     
     public Game1()
     {
@@ -81,71 +57,15 @@ public class Game1 : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
-        playerSprite = Content.Load<Texture2D>("player/player");
-        
-        walkUp = Content.Load<Texture2D>("player/walkUp");
-        walkDown = Content.Load<Texture2D>("player/walkDown");
-        walkLeft = Content.Load<Texture2D>("player/walkLeft");
-        walkRight = Content.Load<Texture2D>("player/walkRight");
-        
-        walk_up_atlas = Texture2DAtlas.Create("player/walkUpAtlas", walkUp, 96, 96);
-        walk_down_atlas = Texture2DAtlas.Create("player/walkDownAtlas", walkDown, 96, 96);
-        walk_left_atlas = Texture2DAtlas.Create("player/walkLeftAtlas", walkLeft, 96, 96);
-        walk_right_atlas = Texture2DAtlas.Create("player/walkRightAtlas", walkRight, 96, 96);
+        _background = Content.Load<Texture2D>("background");
 
-        // Walk Up Animation
-        walk_up_sprite_sheet = new SpriteSheet("SpriteSheet/walkup", walk_up_atlas);
-        walk_up_sprite_sheet.DefineAnimation("walkup", builder =>
-        {
-            builder.IsLooping(true)
-                .AddFrame(walk_up_atlas.GetRegion(0).Name, TimeSpan.FromSeconds(0.2))
-                .AddFrame(walk_up_atlas.GetRegion(1).Name, TimeSpan.FromSeconds(0.2))
-                .AddFrame(walk_up_atlas.GetRegion(2).Name, TimeSpan.FromSeconds(0.2))
-                .AddFrame(walk_up_atlas.GetRegion(3).Name, TimeSpan.FromSeconds(0.2));
-        });
-        SpriteSheetAnimation walkUpAnimation = walk_up_sprite_sheet.GetAnimation("walkup");
-        walkUpAnimationController = new AnimationController(walkUpAnimation);
-        
-        // Walk Down Animation
-        walk_down_sprite_sheet = new SpriteSheet("SpriteSheet/walkdown", walk_down_atlas);
-        walk_down_sprite_sheet.DefineAnimation("walkdown", builder =>
-        {
-            builder.IsLooping(true)
-                .AddFrame(walk_down_atlas.GetRegion(0).Name, TimeSpan.FromSeconds(0.2))
-                .AddFrame(walk_down_atlas.GetRegion(1).Name, TimeSpan.FromSeconds(0.2))
-                .AddFrame(walk_down_atlas.GetRegion(2).Name, TimeSpan.FromSeconds(0.2))
-                .AddFrame(walk_down_atlas.GetRegion(3).Name, TimeSpan.FromSeconds(0.2));
-        });
-        SpriteSheetAnimation walkDownAnimation = walk_down_sprite_sheet.GetAnimation("walkdown");
-        walkDownAnimationController = new AnimationController(walkDownAnimation);
+        Texture2D walkUp = Content.Load<Texture2D>("player/walkUp");
+        Texture2D walkDown = Content.Load<Texture2D>("player/walkDown");
+        Texture2D walkLeft = Content.Load<Texture2D>("player/walkLeft");
+        Texture2D walkRight = Content.Load<Texture2D>("player/walkRight");
 
-        // Walk Left Animation
-        walk_left_sprite_sheet = new SpriteSheet("SpriteSheet/walkleft", walk_left_atlas);
-        walk_left_sprite_sheet.DefineAnimation("walkleft", builder =>
-        {
-            builder.IsLooping(true)
-                .AddFrame(walk_left_atlas.GetRegion(0).Name, TimeSpan.FromSeconds(0.2))
-                .AddFrame(walk_left_atlas.GetRegion(1).Name, TimeSpan.FromSeconds(0.2))
-                .AddFrame(walk_left_atlas.GetRegion(2).Name, TimeSpan.FromSeconds(0.2))
-                .AddFrame(walk_left_atlas.GetRegion(3).Name, TimeSpan.FromSeconds(0.2));
-        });
-        SpriteSheetAnimation walkLeftAnimation = walk_left_sprite_sheet.GetAnimation("walkleft");
-        walkLeftAnimationController = new AnimationController(walkLeftAnimation);
+        player = new Player(walkUp, walkDown, walkLeft, walkRight);
 
-        // Walk Right Animation
-        walk_right_sprite_sheet = new SpriteSheet("SpriteSheet/walkright", walk_right_atlas);
-        walk_right_sprite_sheet.DefineAnimation("walkright", builder =>
-        {
-            builder.IsLooping(true)
-                .AddFrame(walk_right_atlas.GetRegion(0).Name, TimeSpan.FromSeconds(0.2))
-                .AddFrame(walk_right_atlas.GetRegion(1).Name, TimeSpan.FromSeconds(0.2))
-                .AddFrame(walk_right_atlas.GetRegion(2).Name, TimeSpan.FromSeconds(0.2))
-                .AddFrame(walk_right_atlas.GetRegion(3).Name, TimeSpan.FromSeconds(0.2));
-        });
-        SpriteSheetAnimation walkRightAnimation = walk_right_sprite_sheet.GetAnimation("walkright");
-        walkRightAnimationController = new AnimationController(walkRightAnimation);
-
-        background = Content.Load<Texture2D>("background");
         ball = Content.Load<Texture2D>("ball");
         skull = Content.Load<Texture2D>("skull");
     }
@@ -158,25 +78,6 @@ public class Game1 : Game
 
         // TODO: Add your update logic here
         _camera.Position = player.Position - new Vector2(_graphics.PreferredBackBufferWidth / 2f, _graphics.PreferredBackBufferHeight / 2f);
-
-        if (player.IsMoving)
-        {
-            switch (player.getDirection())
-            {
-                case Dir.Up:
-                    walkUpAnimationController.Update(gameTime);
-                    break;  
-                case Dir.Down:
-                    walkDownAnimationController.Update(gameTime);
-                    break; 
-                case Dir.Left:
-                    walkLeftAnimationController.Update(gameTime);
-                    break;   
-                case Dir.Right:
-                    walkRightAnimationController.Update(gameTime);
-                    break;   
-            }
-        }
         
         player.Update(gameTime);
         base.Update(gameTime);
@@ -188,57 +89,8 @@ public class Game1 : Game
 
         // TODO: Add your drawing code here
         _spriteBatch.Begin(transformMatrix: this._camera.GetViewMatrix());
-        _spriteBatch.Draw(background, new Vector2(-500, -500), Color.White);
-        if (player.IsMoving)
-        {
-            Texture2DRegion currentWalkFrame;
-            switch (player.getDirection())
-            {
-                case Dir.Up:
-                    walkUpAnimationController.Update(gameTime);
-                    currentWalkFrame= walk_up_sprite_sheet.TextureAtlas[walkUpAnimationController.CurrentFrame];
-                    _spriteBatch.Draw(currentWalkFrame, player.Position - PLAYER_OFFSET, Color.White);
-                    break;  
-                case Dir.Down:
-                    walkDownAnimationController.Update(gameTime);
-                    currentWalkFrame = walk_down_sprite_sheet.TextureAtlas[walkDownAnimationController.CurrentFrame];
-                    _spriteBatch.Draw(currentWalkFrame, player.Position - PLAYER_OFFSET, Color.White);
-                    break; 
-                case Dir.Left:
-                    walkLeftAnimationController.Update(gameTime);
-                    currentWalkFrame = walk_left_sprite_sheet.TextureAtlas[walkLeftAnimationController.CurrentFrame];
-                    _spriteBatch.Draw(currentWalkFrame, player.Position - PLAYER_OFFSET, Color.White);
-                    break;   
-                case Dir.Right:
-                    walkRightAnimationController.Update(gameTime);
-                    currentWalkFrame = walk_right_sprite_sheet.TextureAtlas[walkRightAnimationController.CurrentFrame];
-                    _spriteBatch.Draw(currentWalkFrame, player.Position - PLAYER_OFFSET, Color.White);
-                    break;   
-            }
-        }
-        else
-        {
-            Texture2DRegion currentWalkFrame;
-            switch (player.getDirection())
-            {
-                case Dir.Up:
-                    currentWalkFrame= walk_up_sprite_sheet.TextureAtlas[3];
-                    _spriteBatch.Draw(currentWalkFrame, player.Position - PLAYER_OFFSET, Color.White);
-                    break;  
-                case Dir.Down:
-                    currentWalkFrame = walk_down_sprite_sheet.TextureAtlas[3];
-                    _spriteBatch.Draw(currentWalkFrame, player.Position - PLAYER_OFFSET, Color.White);
-                    break; 
-                case Dir.Left:
-                    currentWalkFrame = walk_left_sprite_sheet.TextureAtlas[3];
-                    _spriteBatch.Draw(currentWalkFrame, player.Position - PLAYER_OFFSET, Color.White);
-                    break;   
-                case Dir.Right:
-                    currentWalkFrame = walk_right_sprite_sheet.TextureAtlas[3];
-                    _spriteBatch.Draw(currentWalkFrame, player.Position - PLAYER_OFFSET, Color.White);
-                    break;   
-            }
-        }
+        _spriteBatch.Draw(_background, new Vector2(-500, -500), Color.White);
+        player.Draw(_spriteBatch);
         _spriteBatch.End();
         
         base.Draw(gameTime);
--- Player.cs
+++ Player.cs
@@ -1,46 +1,100 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Graphics;
 
+using MonoGame.Extended.Animations;
+using MonoGame.Extended.Graphics;
+
 namespace StardewJelly;
 class Player
 {
-    private Vector2 position = new Vector2(500, 300);
-    private int speed = 300;
-    private Dir dir  = Dir.Down;
-    private bool isMoving = false;
+    private readonly Vector2 _playerOffset = new Vector2(48, 48);
+    
+    private Vector2 _position = new Vector2(500, 300);
+    private const int Speed = 300;
+    private Dir _dir  = Dir.Down;
+    private bool _isMoving = false;
+    
+    private readonly SpriteSheet _walkUpSpriteSheet;
+    private readonly SpriteSheet _walkDownSpriteSheet;
+    private readonly SpriteSheet _walkLeftSpriteSheet;
+    private readonly SpriteSheet _walkRightSpriteSheet;
 
-    public Vector2 Position
-    {
-        get => position;
-        set => position = value;
-    }
+    private readonly AnimationController _walkUpAnimationController;
+    private readonly AnimationController _walkDownAnimationController;
+    private readonly AnimationController _walkLeftAnimationController;
+    private readonly AnimationController _walkRightAnimationController;
 
-    public void setX(int x)
+    public Player(Texture2D walkUp, Texture2D walkDown, Texture2D walkLeft, Texture2D walkRight)
     {
-        position.X = x;
-    }
+        Texture2DAtlas walkUpAtlas = Texture2DAtlas.Create("player/walkUpAtlas", walkUp, 96, 96);
+        Texture2DAtlas walkDownAtlas = Texture2DAtlas.Create("player/walkDownAtlas", walkDown, 96, 96);
+        Texture2DAtlas walkLeftAtlas = Texture2DAtlas.Create("player/walkLeftAtlas", walkLeft, 96, 96);
+        Texture2DAtlas walkRightAtlas = Texture2DAtlas.Create("player/walkRightAtlas", walkRight, 96, 96);
 
-    public void setY(int y)
-    {
-        position.Y = y;
-    }
+        // Walk Up Animation
+        _walkUpSpriteSheet = new SpriteSheet("SpriteSheet/walkup", walkUpAtlas);
+        _walkUpSpriteSheet.DefineAnimation("walkup", builder =>
+        {
+            builder.IsLooping(true)
+                .AddFrame(walkUpAtlas.GetRegion(0).Name, TimeSpan.FromSeconds(0.2))
+                .AddFrame(walkUpAtlas.GetRegion(1).Name, TimeSpan.FromSeconds(0.2))
+                .AddFrame(walkUpAtlas.GetRegion(2).Name, TimeSpan.FromSeconds(0.2))
+                .AddFrame(walkUpAtlas.GetRegion(3).Name, TimeSpan.FromSeconds(0.2));
+        });
+        SpriteSheetAnimation walkUpAnimation = _walkUpSpriteSheet.GetAnimation("walkup");
+        
+        // Walk Down Animation
+        _walkDownSpriteSheet = new SpriteSheet("SpriteSheet/walkdown", walkDownAtlas);
+        _walkDownSpriteSheet.DefineAnimation("walkdown", builder =>
+        {
+            builder.IsLooping(true)
+                .AddFrame(walkDownAtlas.GetRegion(0).Name, TimeSpan.FromSeconds(0.2))
+                .AddFrame(walkDownAtlas.GetRegion(1).Name, TimeSpan.FromSeconds(0.2))
+                .AddFrame(walkDownAtlas.GetRegion(2).Name, TimeSpan.FromSeconds(0.2))
+                .AddFrame(walkDownAtlas.GetRegion(3).Name, TimeSpan.FromSeconds(0.2));
+        });
+        SpriteSheetAnimation walkDownAnimation = _walkDownSpriteSheet.GetAnimation("walkdown");
 
-    public Dir getDirection()
-    {
-        return dir;
+        // Walk Left Animation
+        _walkLeftSpriteSheet = new SpriteSheet("SpriteSheet/walkleft", walkLeftAtlas);
+        _walkLeftSpriteSheet.DefineAnimation("walkleft", builder =>
+        {
+            builder.IsLooping(true)
+                .AddFrame(walkLeftAtlas.GetRegion(0).Name, TimeSpan.FromSeconds(0.2))
+                .AddFrame(walkLeftAtlas.GetRegion(1).Name, TimeSpan.FromSeconds(0.2))
+                .AddFrame(walkLeftAtlas.GetRegion(2).Name, TimeSpan.FromSeconds(0.2))
+                .AddFrame(walkLeftAtlas.GetRegion(3).Name, TimeSpan.FromSeconds(0.2));
+        });
+        SpriteSheetAnimation walkLeftAnimation = _walkLeftSpriteSheet.GetAnimation("walkleft");
+
+        // Walk Right Animation
+        _walkRightSpriteSheet = new SpriteSheet("SpriteSheet/walkright", walkRightAtlas);
+        _walkRightSpriteSheet.DefineAnimation("walkright", builder =>
+        {
+            builder.IsLooping(true)
+                .AddFrame(walkRightAtlas.GetRegion(0).Name, TimeSpan.FromSeconds(0.2))
+                .AddFrame(walkRightAtlas.GetRegion(1).Name, TimeSpan.FromSeconds(0.2))
+                .AddFrame(walkRightAtlas.GetRegion(2).Name, TimeSpan.FromSeconds(0.2))
+                .AddFrame(walkRightAtlas.GetRegion(3).Name, TimeSpan.FromSeconds(0.2));
+        });
+        SpriteSheetAnimation walkRightAnimation = _walkRightSpriteSheet.GetAnimation("walkright");
+        
+        _walkUpAnimationController = new AnimationController(walkUpAnimation);
+        _walkDownAnimationController = new AnimationController(walkDownAnimation);
+        _walkLeftAnimationController = new AnimationController(walkLeftAnimation);
+        _walkRightAnimationController = new AnimationController(walkRightAnimation);
     }
     
-    public bool IsMoving
+    public Vector2 Position
     {
-        get => isMoving;
+        get => _position;
+        set => _position = value;
     }
-
+    
     public void Update(GameTime gameTime)
     {
         Keys[] movementKeys =
@@ -56,50 +110,101 @@ class Player
 
         if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
         {
-            dir = Dir.Up;
+            _dir = Dir.Up;
         }
         if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
         {
-            dir = Dir.Down;
+            _dir = Dir.Down;
         }
 
         if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
         {
-            dir = Dir.Left;
+            _dir = Dir.Left;
         }
 
         if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
         {
-            dir = Dir.Right;
+            _dir = Dir.Right;
         }
 
         // Player is moving if any movement key is currently pressed
-        isMoving = movementKeys.Any(k => keyboardState.IsKeyDown(k));
+        _isMoving = movementKeys.Any(k => keyboardState.IsKeyDown(k));
         
-        if (isMoving)
+        if (_isMoving)
         {
-            switch (dir)
+            switch (_dir)
             {
                 case Dir.Up:
-                    if(position.Y > 200)
-                        position.Y -= speed * dt;
+                    _walkUpAnimationController.Update(gameTime);
+                    if(_position.Y > 200)
+                        _position.Y -= Speed * dt;
                     break;
                 case Dir.Down:
-                    if(position.Y < 1250)
-                        position.Y += speed * dt;
-                    isMoving = true;
+                    _walkDownAnimationController.Update(gameTime);
+                    if(_position.Y < 1250)
+                        _position.Y += Speed * dt;
                     break;
                 case Dir.Left:
-                    if(position.X > 225)
-                        position.X -= speed * dt;
-                    isMoving = true;
+                    _walkLeftAnimationController.Update(gameTime);
+                    if(_position.X > 225)
+                        _position.X -= Speed * dt;
                     break;
                 case Dir.Right:
-                    if(position.X < 1275)
-                        position.X += speed * dt;
-                    isMoving = true;
+                    _walkRightAnimationController.Update(gameTime);
+                    if(_position.X < 1275)
+                        _position.X += Speed * dt;
                     break;
             }            
+        }
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    { 
+        if(_isMoving)
+        {
+            Texture2DRegion currentWalkFrame;
+            switch (_dir)
+            {
+                case Dir.Up:
+                    currentWalkFrame= _walkUpSpriteSheet.TextureAtlas[_walkUpAnimationController.CurrentFrame];
+                    spriteBatch.Draw(currentWalkFrame, Position - _playerOffset, Color.White);
+                    break;  
+                case Dir.Down:
+                    currentWalkFrame = _walkDownSpriteSheet.TextureAtlas[_walkDownAnimationController.CurrentFrame];
+                    spriteBatch.Draw(currentWalkFrame, Position - _playerOffset, Color.White);
+                    break; 
+                case Dir.Left:
+                    currentWalkFrame = _walkLeftSpriteSheet.TextureAtlas[_walkLeftAnimationController.CurrentFrame];
+                    spriteBatch.Draw(currentWalkFrame, Position - _playerOffset, Color.White);
+                    break;   
+                case Dir.Right:
+                    currentWalkFrame = _walkRightSpriteSheet.TextureAtlas[_walkRightAnimationController.CurrentFrame];
+                    spriteBatch.Draw(currentWalkFrame, Position - _playerOffset, Color.White);
+                    break;   
+            }
+        }
+        else
+        {
+            Texture2DRegion currentWalkFrame;
+            switch (_dir)
+            {
+                case Dir.Up:
+                    currentWalkFrame= _walkUpSpriteSheet.TextureAtlas[3];
+                    spriteBatch.Draw(currentWalkFrame, Position - _playerOffset, Color.White);
+                    break;  
+                case Dir.Down:
+                    currentWalkFrame = _walkDownSpriteSheet.TextureAtlas[3];
+                    spriteBatch.Draw(currentWalkFrame, Position - _playerOffset, Color.White);
+                    break; 
+                case Dir.Left:
+                    currentWalkFrame = _walkLeftSpriteSheet.TextureAtlas[3];
+                    spriteBatch.Draw(currentWalkFrame, Position - _playerOffset, Color.White);
+                    break;   
+                case Dir.Right:
+                    currentWalkFrame = _walkRightSpriteSheet.TextureAtlas[3];
+                    spriteBatch.Draw(currentWalkFrame, Position - _playerOffset, Color.White);
+                    break;   
+            }
         }
     }
 }
